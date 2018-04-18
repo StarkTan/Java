c8ad7fd8e0740ce5def6100fc6939f83a00bdd35
Stark <386707933@qq.com>
2018-04-18 20:27:56 +0800

    创建工具类

---
+++ DataBase/Hibernate/src/main/java/com/stark/utils/Utils.java
@@ -0,0 +1,12 @@
+package com.stark.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by Stark on 2018/4/18.
+ * 工具类
+ */
+public class Utils {
+
+}
