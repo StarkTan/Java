1456fa05e905d99bf76be9bae561b2c17c0b11cf
Stark <386707933@qq.com>
2018-08-02 19:37:06 +0800

    JVM 学习垃圾回收

---
+++ JVMLearning/src/com/stark/FinalizeEscapeGC.java
@@ -0,0 +1,44 @@
+package com.stark;
+
+/**
+ * 确认CG 调用过程
+ */
+public class FinalizeEscapeGC {
+
+    public static FinalizeEscapeGC SAVE_HOOK = null;
+
+    public void isAlive() {
+        System.out.println("yes, i am still alive:)");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("finalize method executed!");
+        FinalizeEscapeGC.SAVE_HOOK = this;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SAVE_HOOK = new FinalizeEscapeGC();
+
+        // JVM 在CG时会调用对象的 finalize()
+        SAVE_HOOK = null;
+        System.gc();
+        Thread.sleep(500);
+        if(SAVE_HOOK!=null){
+            SAVE_HOOK.isAlive();
+        }else {
+            System.out.println("no i am dead:(");
+        }
+
+        // 但是只会调用一次
+        SAVE_HOOK = null;
+        System.gc();
+        Thread.sleep(500);
+        if(SAVE_HOOK!=null){
+            SAVE_HOOK.isAlive();
+        }else {
+            System.out.println("no i am dead:(");
+        }
+    }
+}
